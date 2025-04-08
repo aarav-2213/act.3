c9ac1cc1f36dda3bcd6f2abe0b7f02b455db8e3c
aarav-2213 <naniangels2213@gmail.com>
2025-04-08 17:35:37 +1000

    Add files via upload

---
+++ acy3.py
@@ -0,0 +1,16 @@
+print("hello this is aarav.\n")
+print("the birthday of my 5 freinds are\n")
+
+
+saiVar="31st October"
+print("sai's birthday is on -'",saiVar)
+oliverVar="12th jan"
+print("olivers birthday is on -'",oliverVar)
+arjunVar="15th march"
+print("arjuns birthday is on -'",arjunVar)
+ryanVar="27th june"
+print("ryans birthday is on-'",ryanVar)
+jacksonVar="2nd september"
+print("jacksons birthday is on-'",jacksonVar)
+nivaanVar="15th may"
+print("nivaans birthday is on -'",nivaanVar)
