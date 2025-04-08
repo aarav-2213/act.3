ee3c59512601c4eae9713569e1f421a0d61cb228
aarav-2213 <naniangels2213@gmail.com>
2025-04-08 17:36:35 +1000

    Add files via upload

---
+++ a2.py
@@ -0,0 +1,11 @@
+num3= 6514.56984
+print("datatype of number:",type(num3))
+num5=int(num3)
+print("datatype of number:",type(num5))
+
+
+
+
+age=int(input("enter your age:"))
+print("your age is " ,age)
+print("datatype of age:",type(age))
---
+++ lesson 3 .py
@@ -0,0 +1,13 @@
+num=65346
+print("datatype of number:" ,type (num))
+
+numhi=365.654
+print("datatype of number:" ,type(numhi))
+
+numlo=False
+print("datatype of number:" ,type (numlo))
+
+
+blow="Aarav"
+print("datatype of number:" ,type(blow))
+
